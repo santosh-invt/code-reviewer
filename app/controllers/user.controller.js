9763b4139f242d5c32c6dafb3e22cac33d781d27
santosh-invt <santosh.yadav307796@gmail.com>
2023-11-21 23:24:16 +0530

    changes added for new revwer

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,8 +1,15 @@
 exports.allAccess = (req, res) => {
+ 
+    var e = 0.017453292519943295;
+    var g = pow(10, 10);
+    var h = ((c - a) * e).abs();
+    var i = ((d - b) * e).abs();
+    var j = (1 - cos(h)) / 2 + cos(a * e) * cos(c * e) * (1 - cos(i)) / 2;
+    
   if(req.body.check) {
-    return res.status(200).send('CHECK PASSED');
+    return res.status(200).send(`CHECK PASSED AND J IS ${j}`);
   } else {
-    res.status(200).send("CHECK NOT PASSED");
+    res.status(200).send(`CHECK NOT PASSED AND J IS ${j}`);
   }
   
 };
